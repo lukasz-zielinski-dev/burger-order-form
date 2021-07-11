ede68a8ea37744e0aeaf8d1f94047e5205f97be9
Łukasz Zieliński <lukasz.zielinski.kontakt@gmail.com>
2021-07-11 14:31:28 +0200

    initial form page layout

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+const materialModules = [MatCardModule];
+
+@NgModule({
+  declarations: [],
+  imports: [...materialModules],
+  exports: [...materialModules],
+})
+export class MaterialModule {}
